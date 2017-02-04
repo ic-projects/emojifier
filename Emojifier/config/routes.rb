7408687e7f6a4ac3194144bae56907cb36190c43
Mike Fournial <mayeul.fournial@outlook.com>
2017-02-04 19:44:10 +0000

    Ajax for enter works

--- Emojifier/config/routes.rb
+++ Emojifier/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'welcome/pyscript'
+  get 'welcome/pysave'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
