b10e1ecfbc6b42107df1c10db177c30664edd6ae
Mike Fournial <mayeul.fournial@outlook.com>
2017-02-04 13:39:10 +0000

    welcome index

--- Emojifier/app/controllers/welcome_controller.rb
+++ Emojifier/app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @output = %x`python lib/assets/py/py_test.py BITACH`
+    @output = %x`python lib/assets/py/py_test.py Comand line`
     render :text => @output
   end
 end
